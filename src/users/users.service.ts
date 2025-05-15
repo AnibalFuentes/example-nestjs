527bea84a5ea393a1ba116371255bbf251788dca
AnibalFuentes <anibaldfuentesprieto@gmail.com>
2025-05-14 21:42:14 -0500

    middleware

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,11 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  private users: any[] = [
-    { id: 1, name: 'User 1', phone: '23423423' },
-    { id: 2, name: 'User 2', phone: '23423423' },
-    { id: 3, name: 'User 3', phone: '23423423' },
-  ];
+  private users: any[] = [];
 
   getAllUsers() {
     return this.users;
